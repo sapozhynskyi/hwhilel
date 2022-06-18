54941ea4e248319b72c399745aef976ec3b67729
Sapozhynskyi <sapozhynskyi@gmail.com>
2022-06-18 18:02:21 +0300

    hw 8 and 9

--- hw8.php
+++ hw8.php
@@ -1,1 +1,58 @@
 <?php
+
+interface SaveProduct{
+    public function save(Product $product);
+}
+interface UpdateProduct{
+    public function update(Product $product);
+}
+interface DeleteProduct{
+    public function delete(Product $product);
+}
+interface ShowProduct{
+    public function show(Product $product);
+}
+interface PrintProduct{
+    public function print(Product $product);
+}
+
+class Product {
+    public function __construct(private string $name, private string $value)
+    {
+    }
+    public function set($name, $value){
+    }
+    public function get($name){
+        return $this->name;
+    }
+}
+class ChangeProduct implements SaveProduct, UpdateProduct, DeleteProduct {
+
+    public function save(Product $product)
+    {
+        // TODO: Implement save() method.
+    }
+    public function update(Product $product)
+    {
+        // TODO: Implement update() method.
+    }
+    public function delete(Product $product)
+    {
+        // TODO: Implement delete() method.
+    }
+}
+
+class DemonstrationProduct implements ShowProduct, PrintProduct {
+
+    public function show(Product $product)
+    {
+        // TODO: Implement show() method.
+    }
+
+    public function print(Product $product)
+    {
+        // TODO: Implement print() method.
+    }
+}
+
+
--- hw9.php
+++ hw9.php
@@ -1,1 +1,89 @@
 <?php
+interface Deliver{
+    public function getDeliver(Logger $logger);
+}
+interface Format{
+    public function getFormat(Logger $logger);
+}
+
+class FormatRaw implements Format{
+
+    public function getFormat(Logger $logger)
+    {
+        return $logger->str;
+    }
+}
+class FormatDateDetails implements Format{
+
+    public function getFormat(Logger $logger)
+    {
+        return date('Y-m-d H:i:s') . $logger->str . ' - With some details';
+    }
+}
+class FormatWithDate implements Format{
+
+    public function getFormat(Logger $logger)
+    {
+        return date('Y-m-d H:i:s') . $logger->str;
+    }
+}
+
+class DeliverEmail implements Deliver {
+
+    public function getDeliver(Logger $logger)
+    {
+        return "Вывод формата ({$logger->format()}) по имейл";
+    }
+}
+
+class DeliverSms implements Deliver {
+
+    public function getDeliver(Logger $logger)
+    {
+        return "Вывод формата ({$logger->format()}) в смс";
+    }
+}
+class DeliverConsole implements Deliver {
+
+    public function getDeliver(Logger $logger)
+    {
+        return "Вывод формата ({$logger->format()}) в консоль";
+    }
+}
+
+
+class Logger
+{
+    public function __construct(Format $format, Deliver $delivery, $str)
+    {   $this->str = $str;
+        $this->format   = $format;
+        $this->delivery = $delivery;
+    }
+
+   public function log()
+    {
+        $this->deliver();
+    }
+
+
+    public function format()
+    {
+        return $this->format->getFormat($this);
+
+    }
+
+    public function deliver()
+    {
+        echo $this->delivery->getDeliver($this);
+
+    }
+
+}
+
+$logger = new Logger(new FormatRaw(), new DeliverEmail(),'fffff');
+$logger->log();
+$logger = new Logger(new FormatWithDate(), new DeliverSms(), 'egergv');
+$logger->log();
+$logger = new Logger(new FormatDateDetails(), new DeliverConsole(),'fwefwef');
+$logger->log();
+
